b0b8d146ed2cf88dd2287539944f3e8ed0e136d0
Rishabh Verma <rishabhverma17@gmail.com>
2017-11-24 14:54:02 +0530

    Script Push

--- Script.py
+++ Script.py
@@ -1,9 +1,8 @@
 import csv
 import re
 
-print("Welcome to SMS Slang Translator")
+print("Welcome to SMS abbreviation Translator")
 print("Remember not to use any void character in continuation")
-print("For Example : Right Input -> brb, Wrong input -> brb!")
 
 print('===================================================')
 
@@ -12,18 +11,18 @@ def translator(user_string):
     j = 0
     for _str in user_string:
         # File path which consists of Abbreviations.
-        fileName = "G:\\Projects [Git]\\sms_slag_translator\\slang.txt"
+        fileName = "C:\\Users\\risha\\Desktop\\slang.txt"
         # File Access mode [Read Mode]
         accessMode = "r"
         with open(fileName, accessMode) as myCSVfile:
-            # Reading file as CSV with delimiter as "=", so that slang are stored in row[0] and abbreviation in row[1]
+            # Reading file as CSV with delimiter as "=", so that abbreviation are stored in row[0] and phrases in row[1]
             dataFromFile = csv.reader(myCSVfile, delimiter="=")
             # Removing Special Characters.
             _str = re.sub('[^a-zA-Z0-9-_.]', '', _str)
             for row in dataFromFile:
                 # Check if selected word matches short forms[LHS] in text file.
                 if _str.upper() == row[0]:
-                    # If match found replace it with its Abbreviation in text file.
+                    # If match found replace it with its appropriate phrase in text file.
                     user_string[j] = row[1]
             myCSVfile.close()
         j = j + 1
