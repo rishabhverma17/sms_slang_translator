6c782f950293a3878e941a8489406b1bbe29a5cc
Rishabh Verma <rishabhverma17@gmail.com>
2017-11-24 12:06:24 +0530

    Python Script Push

---
+++ sms_translator_function.py
@@ -0,0 +1,44 @@
+import csv
+import re
+
+print("Welcome to SMS Slang Translator")
+print("Remember not to use any void character in continuation")
+print("For Example : Right Input -> brb, Wrong input -> brb!")
+
+print('===================================================')
+
+def translator(user_string):
+    user_string = user_string.split(" ")
+    j = 0
+    for _str in user_string:
+        # File path which consists of Abbreviations.
+        fileName = "C:\\Users\\risha\\Desktop\\slang.txt"
+        # File Access mode [Read Mode]
+        accessMode = "r"
+        with open(fileName, accessMode) as myCSVfile:
+            # Reading file as CSV with delimiter as "=", so that slang are stored in row[0] and abbreviation in row[1]
+            dataFromFile = csv.reader(myCSVfile, delimiter="=")
+            # Removing Special Characters.
+            _str = re.sub('[^a-zA-Z0-9-_.]', '', _str)
+            for row in dataFromFile:
+                # Check if selected word matches short forms[LHS] in text file.
+                if _str.upper() == row[0]:
+                    # If match found replace it with its Abbreviation in text file.
+                    user_string[j] = row[1]
+            myCSVfile.close()
+        j = j + 1
+    # Replacing commas with spaces for final output.
+    print(' '.join(user_string))
+    print('===================================================')
+    print('')
+
+while True:
+    print("Provide Input below or print exit or EXIT to end script")
+    # Getting User String.
+    # Sample : user input = "Hi Rishabh meet me asap!"
+    user = input()
+    # Keep Calling procedure until EXIT or exit keyword is encountered.
+    if user.upper() == 'EXIT':
+        print("Exiting Script")
+        break
+    translator(user)
